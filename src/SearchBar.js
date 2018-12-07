d82ac7dbc72ba08f5f25c9f8bd6a24ead4e33d86
Edward Whelan <vroxing@gmail.com>
2018-12-07 14:28:08 -0500

    add comments for search bar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,9 +9,10 @@ class SearchBar extends Component {
       matchedCities: [],
     };
   }
-  componentDidMount() {
-    const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+  componentDidMount() {
+    // get the data from the api => parse to json => set state of cities which is master array to be used.
+    const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
     fetch(endpoint)
       .then(r => r.json())
       .then(r => {
@@ -21,6 +22,7 @@ class SearchBar extends Component {
         })
       })
   }
+
   render() {
     return (
       <div>
@@ -34,37 +36,30 @@ class SearchBar extends Component {
       </div>
     )
   }
-
+  //draw <li> elements to the page maps a passed in array to be sorted
   _makeList(arg) {
     return arg.map((result, index) => {
       return (<li key={index}>{result.city}, {result.state} : pop. {result.population}</li>)
     })
   }
 
-
-
+  //function to find if a passed in arg matches. Uses RegExp to flatten
+  // returns either the city or the state
   _findMatches(toMatch) {
     return this.state.cities.filter(place => {
       const regex = new RegExp(toMatch, 'gi');
       return place.city.match(regex) || place.state.match(regex)
     })
   }
-
+  //on change event handler
   _onChange = (event) => {
-    // console.log(event)
     let normal = event.target.value;
     let returnedSearch = this._findMatches(normal)
-
     this.setState({
       term: normal,
       matchedCities: returnedSearch
     });
-
   }
-
-
-
-
 }
 
 export default SearchBar;
